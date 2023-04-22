b77163001a94818d5bf9e8748fc63dc202bcaebc
lawrence luo <lawrence.luo@yahoo.com>
2023-04-22 14:44:47 -0700

    Add navbar in the app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, OnInit, VERSION, ViewChild} from '@angular/core';
+import {ViewportScroller} from "@angular/common";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'personal-website';
+export class AppComponent implements OnInit{
+  name: string = "hello";
+
+  constructor(private scroller: ViewportScroller,
+              private router: Router) {}
+
+  ngOnInit() {
+    this.router.navigate(["/"]);
+  }
+
+  goToAboutMe(){
+    this.scroller.scrollToAnchor("about-me");
+  }
+
+  goToSkills(){
+    this.scroller.scrollToAnchor("skills");
+  }
+
+  goToExperiences(){
+    this.scroller.scrollToAnchor("experiences");
+  }
 }
